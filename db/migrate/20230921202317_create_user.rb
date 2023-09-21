251233ab4684902ad840bb5493706ab0fa1527dd
Dachrono <andres_zamorano_@hotmail.com>
2023-09-21 17:12:13 -0600

    Update README file data

--- db/migrate/20230921202317_create_user.rb
+++ db/migrate/20230921202317_create_user.rb
@@ -1,14 +1,12 @@
 class CreateUser < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-
       t.string :name, null: false
       t.string :photo
       t.text :bio
       t.integer :posts_counter, default: 0
 
       t.timestamps
-
     end
   end
 end
