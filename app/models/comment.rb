aa8f90d35c97101f29c05e609eaee5ff10d059d6
Dachrono <andres_zamorano_@hotmail.com>
2023-09-22 17:38:40 -0600

    create adittionals custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: "User", foreign_key: "Author_id"
-    belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: 'users_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'posts_id'
+
+  after_save :comments_updater
+  after_destroy :comments_updater
+
+  def comments_updater
+    post.update(comments_counter: post.comments.count)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  has_many :posts, foreign_key: "Author_id"
-  has_many :comments, foreign_key: "Author_id"
-  has_many :likes
+  has_many :posts, class_name: 'Post', foreign_key: 'Author_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'users_id'
+  has_many :likes, class_name: 'Like', foreign_key: 'users_id'
+
+  def post_recent
+    posts.order(created_at: :desc).limit(3)
+  end
 end
