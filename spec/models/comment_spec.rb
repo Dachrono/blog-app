2ac3d30baeaf8ef26ef581b11da564650702223c
Dachrono <andres_zamorano_@hotmail.com>
2023-09-22 22:30:36 -0600

    Solve some rubocop issues

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,17 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-    before :each do
-        @user = User.create(name: 'Andy Zam', bio: 'I am trying to be a good developer')
-        @post = Post.create(author: @user, title: 'First post', text:'This is the first test')
-      end
+  before :each do
+    @user = User.create(name: 'Andy Zam', bio: 'I am trying to be a good developer')
+    @post = Post.create(author: @user, title: 'First post', text: 'This is the first test')
+  end
 
   it "updates post's comments_counter after save" do
     comment = Comment.create(
       author: @user,
       post: @post
     )
-    @post.reload 
+    @post.reload
     expect(@post.comments_counter).to eq(1)
   end
 
@@ -31,15 +31,14 @@ RSpec.describe Comment, type: :model do
   end
 
   it 'should update the comments_counter attribute of a post' do
-    comment = Comment.create(post: @post, author: @user,
-                             text: 'This is the first comment')
-    comment2 = Comment.create(post: @post, author: @user,
-                             text: 'This is the second comment')                         
+    Comment.create(post: @post, author: @user,
+                   text: 'This is the first comment')
+    Comment.create(post: @post, author: @user,
+                   text: 'This is the second comment')
 
     comment.comments_updater
     expected_result = 2
 
     expect(@post.comments_counter).to eq(expected_result)
   end
-
 end
