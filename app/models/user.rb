3b03e6218eb561ceb2a07e1486af18d98afae338
Dachrono <andres_zamorano_@hotmail.com>
2023-09-22 19:05:55 -0600

    Create validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'users_id'
   has_many :likes, class_name: 'Like', foreign_key: 'users_id'
 
+  validates :name, presence: true
+  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
   def post_recent
     posts.order(created_at: :desc).limit(3)
   end
