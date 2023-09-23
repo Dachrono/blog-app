fea9529a28c8476902801c43d225406e58e368e7
Dachrono <andres_zamorano_@hotmail.com>
2023-09-22 19:06:12 -0600

    Create validations for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'posts_id'
   has_many :likes, class_name: 'Like', foreign_key: 'posts_id'
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   after_save :post_updater
   after_destroy :post_updater
 
