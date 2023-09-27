63308136e5c4edb8ccbad9306306fb3dd97e60b7
Dachrono <andres_zamorano_@hotmail.com>
2023-09-27 16:56:40 -0600

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+=begin
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  get '/users/:user_id/posts', to: 'user_posts#index'
+  get '/users/:user_id/posts/:id', to: 'user_posts#show'
+=end
+
+  root 'users#index'
+
+  resources :users, only [:index, :show] do
+    resources :posts, only [:index, :show]
+  end  
+   
 end
