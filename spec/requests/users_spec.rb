c6865644a06bcb334d2145cab2e1aec6e3f588b3
Dachrono <andres_zamorano_@hotmail.com>
2023-09-28 20:05:48 -0600

    Create spec file for user controllers and the test requirements

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
+  before :each do
+    @user = User.create(name: 'Andy Zam', bio: 'I am trying to be a good developer')
+    @url = users_url
+  end
+
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "responds with a successful HTTP status" do
+      get @url
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the index template" do
+      get @url
+      expect(response).to render_template(:index)
+    end
+
+    it "includes correct placeholder text in the response body" do
+      get @url
+      expect(response.body).to include("Here is a list of all users")
+    end
+  end
+
+  describe 'GET /show' do
+    it "responds with a successful HTTP status" do
+      get user_url(@user)
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the show template" do
+      get user_url(@user)
+      expect(response).to render_template(:show)
+    end
+
+    it "includes correct placeholder text in the response body" do
+      get user_url(@user)
+      expect(response.body).to include("Here is a description for one user ")
+    end
   end
 end
