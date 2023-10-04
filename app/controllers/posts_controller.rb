34f0dd80972a519780a09805b8274b3844f79ceb
Dachrono <andres_zamorano_@hotmail.com>
2023-10-04 14:26:03 -0600

    Modify content to complete test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @author = User.find(params[:user_id])
-    @posts = @author.posts
+    @posts = @author.posts.includes(:comments)
   end
 
   def show
