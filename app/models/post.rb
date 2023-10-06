933a5512ee97fba178bbf755671740cbbd6a19d0
Dachrono <andres_zamorano_@hotmail.com>
2023-10-06 15:44:10 -0600

    Add depend destroy to comments and likes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'Author_id'
-  has_many :comments, class_name: 'Comment', foreign_key: 'posts_id'
-  has_many :likes, class_name: 'Like', foreign_key: 'posts_id'
+  has_many :comments, class_name: 'Comment', foreign_key: 'posts_id', dependent: :destroy
+  has_many :likes, class_name: 'Like', foreign_key: 'posts_id', dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
